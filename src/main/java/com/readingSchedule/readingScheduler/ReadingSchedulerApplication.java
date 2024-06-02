e44c507ef8568fb10aad2d714e5443b7df838caa
RohanJasoria <45820363+RohanJasoria@users.noreply.github.com>
2024-06-02 23:56:24 +0530

    Add Success Message Response

--- src/main/java/com/readingSchedule/readingScheduler/ReadingSchedulerApplication.java
+++ src/main/java/com/readingSchedule/readingScheduler/ReadingSchedulerApplication.java
@@ -2,10 +2,14 @@ package com.readingSchedule.readingScheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.readingSchedule.readingScheduler"})
+@EnableWebMvc
 public class ReadingSchedulerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/readingSchedule/readingScheduler/controller/ReadingSchedulerController.java
+++ src/main/java/com/readingSchedule/readingScheduler/controller/ReadingSchedulerController.java
@@ -17,11 +17,13 @@ public class ReadingSchedulerController {
     private SyncUrlInfoService syncUrlInfoService;
 
     @PostMapping(value = "/syncUrlInfo")
-    public MessageResponse syncUrlInfo(@RequestParam UrlInfoRequest urlInfoRequest) {
+    public MessageResponse syncUrlInfo(@RequestBody UrlInfoRequest urlInfoRequest) {
         MessageResponse messageResponse = new MessageResponse();
         try {
             log.info("Request for syncing URL info : {}", urlInfoRequest);
             syncUrlInfoService.syncUrlInfo(urlInfoRequest);
+            String message = "URL Info Synced Successfully";
+            messageResponse.setSuccessMessage(message);
         } catch (Exception e) {
             log.error("Error fetching links for today's reading");
             String message = "Error fetching links for today's reading";
